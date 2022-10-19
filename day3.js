4b212f20c6f39be9b873f9e1fd10a3e31a40edd0
nishanth-murugesan <111063472+nishanth-murugesan@users.noreply.github.com>
2022-10-19 20:06:04 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,19 @@
+const resume = 
+    [
+        {
+    "name": "nishanth",
+    "work":"student"
+},
+    {
+    "qualification":"bsc",
+    "place":"madurai"
+},
+{
+    "father name":"murugesan",
+    "nationality":"indian"
+    
+}
+]
+ console.log(resume);// resume in jason
+
+ 
